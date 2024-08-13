9efeb7c38d76bc6cd0d512a9e51b5aadedf04cae
AnshBansal18 <anshbansal2004@gmail.com>
2024-08-13 08:38:34 +0530

    cors policy update

--- server/server.js
+++ server/server.js
@@ -11,11 +11,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type'],
-}));
+app.use(cors());  
 
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
