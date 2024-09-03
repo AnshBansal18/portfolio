10282c98572df0170159b8db9b29102afb9fed7f
AnshBansal18 <anshbansal2004@gmail.com>
2024-09-03 19:07:44 +0530

    updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,6 @@
   import ErrorPage from './Components/Error';
   import Header from './Components/Header/Header';
   import Footer from './Components/Footer/Footer';
-  import Achievements from './Components/Achievements/Achievement';
   import Home from './Components/Home/Home';
   import About from './Components/About/About';
   import Contact from './Components/Contact/Contact';
@@ -40,7 +39,6 @@
               <Route path="/about" element={<About />} />
               <Route path="/contacts" element={<Contact />} />
               <Route path="/projects" element={<Projects />} />
-              <Route path="/achivements" element={<Achievements />} />
               <Route path="/login" element={<LoginForm />} />
               <Route
                 path="/admin"
